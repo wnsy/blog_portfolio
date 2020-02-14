f16bd462ea571eda921406b07c09a08f009daa6b
Ayesha W <ayesha.wan@youdo.co.nz>
2020-02-14 21:46:58 +1300

    Adds nested attrs for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,6 +2,8 @@ class Portfolio < ApplicationRecord
   include Placeholder
 
   has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
 
   validates_presence_of :title, :body, :main_image, :thumbnail
   after_initialize :set_defaults

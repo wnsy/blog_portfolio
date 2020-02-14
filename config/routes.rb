c0d7233ef8c1aee29326697632a5a0ffafb319c2
Ayesha W <ayesha.wan@youdo.co.nz>
2020-02-15 12:39:51 +1300

    Changes registration, login, logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # path: '' so that it won't show as /users/login but as /login
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show]
   get 'rails-projects', to: 'portfolios#rails'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'

89ac8f11d8370c84372325d6bbc329a99ec2e6dc
Ayesha W <ayesha.wnz@gmail.com>
2020-02-18 16:01:39 +1300

    Creates a copyright html generator tool

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   include SetSourceConcern
   include GuestUserConcern
   include DefaultPageContentConcern
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = OthersideCopyrightTool::Render.copyright 'Other-Side.net', 'All rights reserved'
+  end
+end
+
+module OthersideCopyrightTool
+  class Render
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
